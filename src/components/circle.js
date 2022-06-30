2689e03448857af337e912efaf56dd332ad40465
Sourav Banerjee <rjsourav88@gmail.com>
2022-06-30 16:56:00 +0530

    Initital commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,8 @@
 // import using require
-
+var Shape = require("./shape");
 // declare class
-
-
+class Circle extends Shape {
+    constructor() { super(); this.calculateArea = () => { } }
+};
+module.exports = Circle;
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
-
+class Shape {
+    constructor() {
+        this.color = "red";
+    }
+    drawShape() { }
+    calculateArea() { }
+};
+module.exports = Shape;
 // export class using module.exports
